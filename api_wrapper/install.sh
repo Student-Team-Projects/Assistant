c01293c02f6fefc87232df30d571d2d8aff2f0f6
Michał Bawołek <115341532+michalbaw@users.noreply.github.com>
2024-12-03 12:01:50 +0100

    Minor changes to install.sh

--- api_wrapper/install.sh
+++ api_wrapper/install.sh
@@ -1,5 +1,4 @@
 sudo pacman -S --needed autoconf automake autoconf-archive git cmake gcc zip unzip tar
-curl -fsSL https://ollama.com/install.sh | sh
 git clone https://github.com/microsoft/vcpkg
 cd vcpkg
 ./bootstrap-vcpkg.sh
@@ -7,6 +6,7 @@ cd vcpkg
 ./vcpkg install restc-cpp
 ./vcpkg install nlohmann-json
 cd ..
+curl -fsSL https://ollama.com/install.sh | sh
 curl http://localhost:11434/api/pull -d '{
   "model": "llama3.2",
   "stream": false
