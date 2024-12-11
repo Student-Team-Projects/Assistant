18840831b5b54d50610f2762c266d6fedd88f048
anna <annabazan3@gmail.com>
2024-12-11 18:23:28 +0100

    Enter is executing command

--- command_handler/menu_prompt.cpp
+++ command_handler/menu_prompt.cpp
@@ -6,8 +6,13 @@ using namespace std;
 int main() {
     string user_input;
 
-    cout << "Do you want to execute the command? [yes/no]: ";
-    cin >> user_input;
+    cout << "Do you want to execute the command? [yes[Enter]/no]: ";
+    getline(cin, user_input);
+
+    if(user_input.empty()){
+        cout<<"Command confirmed for execution." << endl;
+        return 0;
+    }
 
     for (auto &c : user_input) {
         c = tolower(c);
